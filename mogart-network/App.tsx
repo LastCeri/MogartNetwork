33b55691085f19fa514e44d11a6d8deb3e2ac525
LastCeri <125711498+LastCeri@users.noreply.github.com>
2023-10-30 23:57:04 +0300

    Initial commit

---
+++ mogart-network/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './Pages/home';
+import About from './Pages/about';
+import Signin from './Pages/sing-in';
+import Register from './Pages/register';
+import AdminPanel from './AdminBase/Admin/AdminPanel';
+import Uipanel from './Ui';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sing-in" element={<Signin />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/Mogartadmin" element={<AdminPanel />} />
+        <Route path="/Ui" element={<Uipanel />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
