7599cdd17a94a94eba89125ccbc9289187b58290
TUsluRa <125711498+LastCeri@users.noreply.github.com>
2023-11-05 00:11:43 +0300

    Add Pages <#1>

---
+++ mogart-network/src/Pages/Home/home.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Header from '../../MogartBase/ThemeParts/MainPart/Header/HeaderPart';
+import PopUp from '../../MogartBase/ThemeParts/Inpart/Menus/PopupMenu/PopupMenu';
+import CreatePost from '../../MogartBase/ThemeParts/Inpart/PostArea/CreatePostArea/CreatePostArea';
+import PostContent from '../../MogartBase/ThemeParts/Inpart/PostArea/PostContent/PostContent';
+import SearchBox from '../../MogartBase/ThemeParts/Inpart/SearchBox/SearchBox';
+import Groups from '../../MogartBase/ThemeParts/Inpart/Groups/Groups';
+import RightBar from '../../MogartBase/ThemeParts/Inpart/Right-Bar/RightBar';
+import SecondaryBar from '../../MogartBase/ThemeParts/Inpart/SecondaryBar/SecondaryBar';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import './home.css';
+
+function Home() {
+  return (
+    <div className='Home-Container'>
+      <Header />
+      <PopUp />
+      <CreatePost />
+      <PostContent />
+      <RightBar />
+      <SecondaryBar />
+    </div>
+  );
+}
+
+export default Home;
