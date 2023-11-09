885e3ccf1d930e176df92a607b1c5c85b15b11c0
TUsluRa <125711498+LastCeri@users.noreply.github.com>
2023-11-09 17:53:25 +0300

    Update App.tsx

--- mogart-network/src/App.tsx
+++ mogart-network/src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home/home.tsx';
 import About from './Pages/About/about.tsx';
-import AdminPanel from './AdminBase/Admin/AdminPanel';
 import Groups from './Pages/Groups/GroupsPage.tsx';
 import Login from './Pages/Login/LoginPage.tsx';
 import Signin from './Pages/Sign-in/SignPage.tsx';
@@ -18,7 +17,6 @@ function App() {
         <Route path="/global" element={<Global />} />
         <Route path="/Login" element={<Login />} />
         <Route path="/Sign" element={<Signin />} />
-        <Route path="/Mogartadmin" element={<AdminPanel />} />
       </Routes>
     </Router>
   );
