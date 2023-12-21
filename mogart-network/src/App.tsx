56734918b129e0e8277243d5fd198b324e59a6ae
TUsluRa <125711498+LastCeri@users.noreply.github.com>
2023-12-21 21:27:38 +0300

    Page Update [17] | Update App.tsx

--- mogart-network/src/App.tsx
+++ mogart-network/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home/home.tsx';
-import About from './Pages/About/about.tsx';
+import About from './Pages/About/About.tsx';
 import Groups from './Pages/Groups/GroupsPage.tsx';
 import Login from './Pages/Login/LoginPage.tsx';
 import Profile from './Pages/Profile/Profile.tsx';
@@ -13,6 +13,7 @@ import SettingsPage from './Pages/Settings/SettingsPage.tsx';
 import MessagePage from './Pages/Message/MessagePage.tsx';
 import NotificationsPage from './Pages/Notifications/NotificationsPage.tsx';
 import ActivityPage from './Pages/Activity/ActivityPage.tsx';
+import Global from './Pages/Global/GlobalPage.tsx';
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
         <Route path="/About" element={<About />} />
         <Route path="/Groups" element={<Groups />} />
         <Route path="/Blog" element={<Blogpage />} />
+        <Route path="/Global" element={<Global />} />
 
         <Route path="/Profile" element={<Profile />} />
         <Route path="/Messages" element={<MessagePage />} />
--- mogart-network/src/Pages/Global/GlobalPage.tsx
+++ mogart-network/src/Pages/Global/GlobalPage.tsx
@@ -1,35 +1,49 @@
-import React, { useState, useEffect } from 'react';
-import './GlobalPage.css'; 
-import PopUp from '../../MogartBase/ThemeParts/Inpart/PopupMenu/PopupMenu';
-import GlobalPage from '../../MogartBase/ThemeParts/Inpart/Global/GlobalPage';
-import RightBar from '../../MogartBase/ThemeParts/Inpart/Right-Bar/RightBar';
+import React from 'react';
+import Header from '../../MogartBase/ThemeParts/MainPart/Header/HeaderPart';
+import Navbar from '../../MogartBase/ThemeParts/MainPart/Navbar/Navbar';
 
+const newsItems = [
+  {
+    id: 1,
+    title: "Innovative SocialFi Platforms",
+    description: "Exploring the impact of decentralized finance on social networks.",
+    imageUrl: "https://via.placeholder.com/150"
+  },
+  {
+    id: 2,
+    title: "Mina Protocol Updates",
+    description: "Latest updates and features in the Mina Protocol.",
+    imageUrl: "https://via.placeholder.com/150"
+  },
+];
 
 const Global: React.FC = () => {
     return (
-        <div className="Main-Container">
-          <header className="Main-Header">
-          <div className="header">
-            <span className="social-score">Social Score</span>
-            <span className="title">Mogart Network</span>
-            <button className="menu-btn">...</button>
-            </div>
-          </header>
-          <div className="Main-Content">
-            <aside className="Main-Content-Left">
-              <nav>
-              <PopUp />
-              </nav>
+      <>
+        <Header />
+        <Navbar />
+        <div className="flex flex-col min-h-screen bg-gray-100">
+          <div className="flex flex-1">
+            <aside className="w-1/4 bg-white p-4 border-r">
             </aside>
-            <main className="Main-Content-Center">
-                <GlobalPage />
+            <main className="flex-1 p-4">
+              <h2 className="text-2xl font-bold text-gray-800 mb-4">Special News</h2>
+              <div className="space-y-4">
+                {newsItems.map(newsItem => (
+                  <div key={newsItem.id} className="p-4 bg-white rounded shadow">
+                    <img src={newsItem.imageUrl} alt={newsItem.title} className="w-full h-64 object-cover rounded mb-4"/>
+                    <h3 className="text-xl font-semibold mb-2">{newsItem.title}</h3>
+                    <p>{newsItem.description}</p>
+                  </div>
+                ))}
+              </div>
             </main>
-            <aside className="Main-Content-Right">
-                <RightBar />
+            <aside className="w-1/4 bg-white p-4 border-l">
             </aside>
           </div>
         </div>
-      );
+      </>
+    );
 };
 
 export default Global;
