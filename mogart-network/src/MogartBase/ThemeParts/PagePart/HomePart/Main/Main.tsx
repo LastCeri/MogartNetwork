5835a8a1ca4230d104328b293f81325f3766c6c5
TUsluRa <125711498+LastCeri@users.noreply.github.com>
2023-12-24 21:48:17 +0300

    Page Update [21] | Part Update [13] | Add ThemeParts/PagePart [10]

--- mogart-network/src/MogartBase/ThemeParts/PagePart/HomePart/Main/Main.tsx
+++ mogart-network/src/MogartBase/ThemeParts/PagePart/HomePart/Main/Main.tsx
@@ -87,11 +87,11 @@ function MainContent() {
       ];
 
   return (
-    <main className="flex-1 p-4">
+    <main className=" w-3/6 top-10">
       <div className="bg-white p-4 rounded-lg shadow mb-4 flex items-center space-x-4">
         <img className="h-10 w-10 rounded-full" src="https://cdn.discordapp.com/attachments/1178319248012095509/1178328436557754430/Profile-2.png?ex=658833df&is=6575bedf&hm=2c773dd7496f21049256bf677a9a71a7ca0d8deb20a4eb3a818750040c409f59&" alt="User Avatar" />
         <input 
-          className="form-input flex-1 p-2 border rounded-md" 
+          className="form-input p-2 w-2/4 border rounded-md" 
           type="text" 
           placeholder="What's on your mind?" 
         />
