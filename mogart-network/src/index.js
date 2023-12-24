0596f35b467eb1881bfb87b5131b779e7fb46922
TUsluRa <125711498+LastCeri@users.noreply.github.com>
2023-12-24 21:51:51 +0300

    Add ContextApi/DataProvider

--- mogart-network/src/index.js
+++ mogart-network/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import reportWebVitals from './reportWebVitals';
+import { DataProvider } from './MogartBase/Context/DataContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+  <DataProvider>
     <App />
-  </React.StrictMode>
+  </DataProvider>
+</React.StrictMode>
 );
 
 reportWebVitals();
