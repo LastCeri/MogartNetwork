e127007ccc8d31424891404a7650c0f3521c1e25
TUsluRa <125711498+LastCeri@users.noreply.github.com>
2024-02-17 01:48:50 +0300

    Page Update [57]
    Within the scope of UI/UX Design Refresh (Phase 5): Page Update [57]
    
    I revamped the design to allow users to dynamically access categorized settings in a tabbed structure on the Settings page.

--- mogart-network/src/Pages/Settings/SettingsPage.tsx
+++ mogart-network/src/Pages/Settings/SettingsPage.tsx
@@ -5,7 +5,6 @@ import Header from '../../MogartBase/ThemeParts/MainPart/Header/HeaderPart';
 import Navbar from '../../MogartBase/ThemeParts/MainPart/Navbar/Navbar';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
-import Select from 'react-select';
 
 const languageOptions = [
   { value: 'en', label: 'English' },
@@ -28,6 +27,12 @@ const ProfileSettingsPage = () => {
   const [selectedLanguage, setSelectedLanguage] = useState(languageOptions[0]);
   const [selectedTheme, setSelectedTheme] = useState(themeOptions[0]);
   const [emailVerified, setEmailVerified] = useState(false);
+  const [profileVisibility, setProfileVisibility] = useState('public');
+  const [activityStatus, setActivityStatus] = useState('visible');
+  const [appNotifications, setAppNotifications] = useState(true);
+  const [emailNotifications, setEmailNotifications] = useState(true);
+  const [twoFactorAuthentication, setTwoFactorAuthentication] = useState(false);
+
 
   useEffect(() => {
     if (isLoading) return;
@@ -64,6 +69,20 @@ const ProfileSettingsPage = () => {
     reader.readAsDataURL(file);
   };
 
+  const handlePrivacySettingsSubmit = (e:any) => {
+    e.preventDefault();
+    console.log('Privacy Settings Saved:', { profileVisibility, activityStatus });
+  };
+  const handleNotificationsSettingsSubmit = (e:any)  => {
+    e.preventDefault();
+    console.log('Notification Settings Saved:', { appNotifications, emailNotifications });
+  };
+  
+  const handleSecuritySettingsSubmit = (e:any) => {
+    e.preventDefault();
+    console.log('Security Settings Saved:', { twoFactorAuthentication });
+  };
+  
   return (
     <>
       <Header />
@@ -78,8 +97,8 @@ const ProfileSettingsPage = () => {
                 <Tab>Privacy</Tab>
                 <Tab>Notifications</Tab>
                 <Tab>Security</Tab>
+                <Tab>Wallet</Tab>
               </TabList>
-
               <TabPanel>
                 <form onSubmit={handleFormSubmit}>
                   <div className="space-y-4">
@@ -89,7 +108,6 @@ const ProfileSettingsPage = () => {
                         <input type="file" id="profileImageUpload" className="hidden" onChange={handleProfileImageChange} />
                       </label>
                     </div>
-
                     <div>
                       <label htmlFor="username" className="text-lg text-gray-600">Visible Username</label>
                       <input
@@ -102,7 +120,6 @@ const ProfileSettingsPage = () => {
                         onChange={(e) => setVisibleUsername(e.target.value)}
                       />
                     </div>
-
                     <div>
                       <label htmlFor="bio" className="text-lg text-gray-600">User Biography</label>
                       <textarea
@@ -114,7 +131,6 @@ const ProfileSettingsPage = () => {
                         onChange={(e) => setUserBio(e.target.value)}
                       />
                     </div>
-
                     <div>
                       <label htmlFor="walletAddress" className="text-lg text-gray-600">Wallet Address</label>
                       <input
@@ -127,7 +143,6 @@ const ProfileSettingsPage = () => {
                         onChange={(e) => setWalletAddress(e.target.value)}
                       />
                     </div>
-
                     <div>
                       <label htmlFor="email" className="text-lg text-gray-600">Email Address</label>
                       <input
@@ -152,6 +167,123 @@ const ProfileSettingsPage = () => {
                   </div>
                 </form>
               </TabPanel>
+              <TabPanel>
+                  <div className="p-4">
+                    <h2 className="text-lg font-semibold text-gray-800 mb-4">Privacy Settings</h2>
+                    <form onSubmit={handlePrivacySettingsSubmit}>
+                      <div className="mb-4">
+                        <label htmlFor="profileVisibility" className="block text-sm font-medium text-gray-700">Profile Visibility</label>
+                        <select
+                          id="profileVisibility"
+                          name="profileVisibility"
+                          className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+                          value={profileVisibility}
+                          onChange={(e) => setProfileVisibility(e.target.value)}
+                        >
+                          <option value="public">Public</option>
+                          <option value="friends">Friends</option>
+                          <option value="onlyMe">Only Me</option>
+                        </select>
+                      </div>
+                      <div className="mb-4">
+                        <label htmlFor="activityStatus" className="block text-sm font-medium text-gray-700">Activity Status</label>
+                        <select
+                          id="activityStatus"
+                          name="activityStatus"
+                          className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+                          value={activityStatus}
+                          onChange={(e) => setActivityStatus(e.target.value)}
+                        >
+                          <option value="visible">Visible to everyone</option>
+                          <option value="friends">Visible to friends only</option>
+                          <option value="hidden">Hidden from everyone</option>
+                        </select>
+                      </div>
+                      <div className="flex justify-end">
+                        <button
+                          type="submit"
+                          className="px-6 py-2 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:text-md"
+                        >
+                          Save Privacy Settings
+                        </button>
+                      </div>
+                    </form>
+                  </div>
+                </TabPanel>
+                <TabPanel>
+                  <div className="p-4">
+                    <h2 className="text-lg font-semibold text-gray-800 mb-4">Notification Settings</h2>
+                    <form onSubmit={handleNotificationsSettingsSubmit}>
+                      <div className="mb-4">
+                        <label className="flex items-center">
+                          <input type="checkbox" checked={appNotifications} onChange={(e) => setAppNotifications(e.target.checked)} className="mr-2" />
+                          App Notifications
+                        </label>
+                      </div>
+                      <div className="mb-4">
+                        <label className="flex items-center">
+                          <input type="checkbox" checked={emailNotifications} onChange={(e) => setEmailNotifications(e.target.checked)} className="mr-2" />
+                          Email Notifications
+                        </label>
+                      </div>
+                      <button type="submit" className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">Save Notification Settings</button>
+                    </form>
+                  </div>
+                </TabPanel>
+                <TabPanel>
+                  <div className="p-4">
+                    <h2 className="text-lg font-semibold text-gray-800 mb-4">Security Settings</h2>
+                    <div className="mb-4">
+                      <label className="block text-sm font-medium text-gray-700">Email Verification</label>
+                      <div className="mt-2">
+                        {emailVerified ? (
+                          <p className="text-green-600">Your email is verified.</p>
+                        ) : (
+                          <button
+                            className="px-4 py-2 border border-transparent text-base font-medium rounded-md text-white bg-green-600 hover:bg-green-700"
+                          >
+                            Verify Email
+                          </button>
+                        )}
+                      </div>
+                    </div>
+                    <form onSubmit={handleSecuritySettingsSubmit}>
+                      <div className="mb-4">
+                        <label className="flex items-center">
+                          <input type="checkbox" checked={twoFactorAuthentication} onChange={(e) => setTwoFactorAuthentication(e.target.checked)} className="mr-2" />
+                          Two Factor Authentication
+                        </label>
+                      </div>
+                      <button type="submit" className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">Save Security Settings</button>
+                    </form>
+                  </div>
+                </TabPanel>
+                <TabPanel>
+                  <div className="p-4">
+                    <h2 className="text-lg font-semibold text-gray-800 mb-4">Wallet Settings</h2>
+                    <div className="mb-4">
+                      <label className="block text-sm font-medium text-gray-700">Current Wallet Address</label>
+                      <div className="mt-1 mb-4">
+                        <input
+                          type="text"
+                          readOnly
+                          value={walletAddress}
+                          className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"/>
+                        <button
+                        
+                          className="mt-2 px-4 py-2 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700">
+                          Copy Address
+                        </button>
+                      </div>
+                    </div>
+                    <div className="mb-4">
+                      <label className="block text-sm font-medium text-gray-700">Change Wallet</label>
+                      <button className="mt-2 px-4 py-2 border border-transparent text-base font-medium rounded-md text-white bg-green-600 hover:bg-green-700">
+                        Connect to a Different Wallet
+                      </button>
+                    </div>
+                  </div>
+                </TabPanel>
             </Tabs>
           </div>
         </main>
