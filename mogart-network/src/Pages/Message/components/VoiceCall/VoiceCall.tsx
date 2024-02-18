d1ce6251d0537e3634422c43dd63e7e7933b78e3
LastCeri <125711498+LastCeri@users.noreply.github.com>
2024-02-18 22:45:07 +0300

    Page Update [61] | Voice Chat Update [2] | Add Message\components VoiceCallModal
    Within the scope of UI/UX Design Refresh (Phase 6) and Voice Chat Technology Implementation:
    Page Update [61] and Voice Chat Update [2]: have been completed. I updated the design of the voice chat component on the communication page and added a sidebar named VoiceCallSidebar to provide quick access to the voice chat screen. Then, I added a range input to adjust the voice detection level with the voice chat settings button on the sidebar. Testing for voice chat is still ongoing. It can be tested to see how the design functions.

--- mogart-network/src/Pages/Message/components/VoiceCall/VoiceCall.tsx
+++ mogart-network/src/Pages/Message/components/VoiceCall/VoiceCall.tsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import VoiceCallSidebar from '../VoiceCallSidebar/VoiceCallSidebar';
-import { faPhoneSlash, faMicrophone, faMicrophoneSlash, faHeadphones, faHeadphonesAlt } from '@fortawesome/free-solid-svg-icons';
+import { faPhoneSlash} from '@fortawesome/free-solid-svg-icons';
+import UserCallModal from '../VoiceCallModal/VoiceCallModal';
 
 interface VoiceCallModalProps {
   isCalling: boolean;
@@ -21,10 +22,6 @@ const VoiceCallModal: React.FC<VoiceCallModalProps> = ({
   const [isMicMuted, setIsMicMuted] = useState(false);
   const [isHeadphonesMuted, setIsHeadphonesMuted] = useState(false);
   const [callDuration, setCallDuration] = useState(0); 
-
-  const toggleMic = () => setIsMicMuted(!isMicMuted);
-  const toggleHeadphones = () => setIsHeadphonesMuted(!isHeadphonesMuted);
-
 
   useEffect(() => {
     let interval:any;
@@ -59,22 +56,15 @@ const VoiceCallModal: React.FC<VoiceCallModalProps> = ({
           <div className="flex space-x-4 justify-center">
             {callStatus === 'Chat Connection Started' && (
               <>  
-                <button onClick={toggleMic} className={`flex items-center ${isMicMuted ? 'bg-gray-400 hover:bg-gray-500' : 'bg-green-500 hover:bg-green-600'} text-white font-bold py-2 px-4 rounded-full shadow-lg hover:shadow-xl transition duration-150 ease-in-out`}>
-                  <FontAwesomeIcon icon={isMicMuted ? faMicrophoneSlash : faMicrophone} className="mr-2" />
-                  {isMicMuted ? 'Unmute Mic' : 'Mute Mic'}
-                </button>
                 <button onClick={() => setIsCalling(false)} className="flex items-center bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full shadow-lg hover:shadow-xl transition duration-150 ease-in-out">
                   <FontAwesomeIcon icon={faPhoneSlash} className="mr-2" />
                   End Call
-                </button>
-                <button onClick={toggleHeadphones} className={`flex items-center ${isHeadphonesMuted ? 'bg-gray-400 hover:bg-gray-500' : 'bg-blue-500 hover:bg-blue-600'} text-white font-bold py-2 px-4 rounded-full shadow-lg hover:shadow-xl transition duration-150 ease-in-out`}>
-                  <FontAwesomeIcon icon={isHeadphonesMuted ? faHeadphonesAlt : faHeadphones} className="mr-2" />
-                  {isHeadphonesMuted ? 'Unmute Headphones' : 'Mute Headphones'}
                 </button>
               </>
             )}
           </div>
         </div>
+        <UserCallModal />
       </div>
       </>
     )
--- mogart-network/src/Pages/Message/components/VoiceCallSidebar/VoiceCallSidebar.tsx
+++ mogart-network/src/Pages/Message/components/VoiceCallSidebar/VoiceCallSidebar.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { ChangeEvent, useState } from 'react';
+import { useData } from '../../../../MogartBase/Context/DataContext';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStickyNote, faSearch, faAdjust } from '@fortawesome/free-solid-svg-icons';
+import { faStickyNote, faSearch, faAdjust, faGear } from '@fortawesome/free-solid-svg-icons';
 
 const VoiceCallSidebar: React.FC = () => {
+  const [isSettingsVisible, setIsSettingsVisible] = useState(false);
+  const { voiceDetectionLevel, setVoiceDetectionLevel } = useData(); 
+
+  const toggleSettings = () => {
+    setIsSettingsVisible(!isSettingsVisible);
+  };
+
+  const handleDetectionLevelChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const newLevel = Number(e.target.value);
+    setVoiceDetectionLevel(newLevel);
+  };
+
   return (
-    <div className="fixed left-0 top-50 z-50 p-4 bg-white bg-opacity-90 shadow-xl rounded-r-3xl flex flex-col items-center justify-start space-y-4">
+  <>
+    <div className="fixed left-0 top-1/2 transform -translate-y-1/2 z-50 p-4 bg-white bg-opacity-90 shadow-xl rounded-r-3xl flex flex-col items-center justify-start space-y-4">
       <button className="p-3 bg-blue-500 text-white rounded-full hover:bg-blue-600 transition duration-150 ease-in-out">
         <FontAwesomeIcon icon={faStickyNote} size="lg" />
       </button>
@@ -14,7 +28,30 @@ const VoiceCallSidebar: React.FC = () => {
       <button className="p-3 bg-gray-500 text-white rounded-full hover:bg-gray-600 transition duration-150 ease-in-out">
         <FontAwesomeIcon icon={faAdjust} size="lg" />
       </button>
+      <button onClick={toggleSettings} className="p-3 bg-gray-500 text-white rounded-full hover:bg-gray-600 transition duration-150 ease-in-out">
+        <FontAwesomeIcon icon={faGear} size="lg" />
+      </button>
     </div>
+          {isSettingsVisible && (
+           <div className="fixed left-20 top-1/2 transform -translate-y-1/2 z-50 p-4 bg-white bg-opacity-90 shadow-xl rounded-r-3xl flex flex-col items-center justify-start space-y-4">
+              <h3 className="text-lg font-semibold">Settings</h3>
+              <label htmlFor="detectionLevel" className="block mt-4">
+                Voice Sensivity:
+                <input
+                  type="range"
+                  id="detectionLevel"
+                  name="detectionLevel"
+                  min="5"
+                  max="100"
+                  value={voiceDetectionLevel}
+                  onChange={handleDetectionLevelChange}
+                  className="w-full"
+                />
+              </label>
+              <p>{voiceDetectionLevel}</p>
+            </div>
+          )}
+          </>
   );
 };
 
