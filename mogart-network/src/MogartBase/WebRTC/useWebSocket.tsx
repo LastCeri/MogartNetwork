2e502eb71b18968da9910c494706f51c99d36c2f
LastCeri <125711498+LastCeri@users.noreply.github.com>
2024-03-10 21:29:48 +0300

    Add VoiceChat\components CallFriendsModal, IncomingCallModal| Update WebSocket
    Within the UI/UX Design Overhaul (Phase 8), I added CallFriendsModal and IncomingCallModal components under VoiceChat\components. CallFriendsModal allows users to select appropriate contacts for voice calls, while IncomingCallModal handles incoming call notifications. Additionally, I made structural adjustments for WebSocket functionality as part of the Update WebSocket scope.

--- mogart-network/src/MogartBase/WebRTC/useWebSocket.tsx
+++ mogart-network/src/MogartBase/WebRTC/useWebSocket.tsx
@@ -1,7 +1,9 @@
 // useWebSocket.tsx
 import { useEffect, useRef, useState, useCallback } from 'react';
+import axios from 'axios';
+import { API_URL } from '../Api/Api';
 
-const useWebSocket = (url:any, onMessage:any) => {
+const useWebSocket = (url: string, onMessage: (event: MessageEvent) => void) => {
     const webSocketRef = useRef<WebSocket | null>(null);
     const [isConnected, setIsConnected] = useState(false);
 
@@ -12,28 +14,42 @@ const useWebSocket = (url:any, onMessage:any) => {
     }, []);
 
     useEffect(() => {
-        const ws = new WebSocket(url);
-
-        ws.onopen = () => {
-            setIsConnected(true);
-            console.log("WebSocket connection established");
-        };
-
-        ws.onmessage = (event) => {
-            if(onMessage) {
-                onMessage(event);
+        const initWebSocket = async () => {
+            try {
+                const response = await axios.get(`${API_URL}/RpcServerStatus`); 
+                console.log("RpcServerStatus",response);
+                if (response.statusText === "on") {
+                    const ws = new WebSocket(url);
+                   
+                    ws.onopen = () => {
+                        setIsConnected(true);
+                        console.log("WebSocket connection established");
+                    };
+                    
+                    ws.onmessage = (event) => {
+                        onMessage(event);
+                    };
+                    
+                    ws.onclose = () => {
+                        setIsConnected(false);
+                        console.log("WebSocket connection closed");
+                    };
+                    
+                    webSocketRef.current = ws;
+                } else {
+                    console.log("Voice Chat Server Disabled", response.status);
+                }
+            } catch (error) {
+                console.error("Error initializing WebSocket:", error);
             }
         };
 
-        ws.onclose = () => {
-            setIsConnected(false);
-            console.log("WebSocket connection closed");
-        };
-
-        webSocketRef.current = ws;
+        initWebSocket();
 
         return () => {
-            ws.close();
+            if (webSocketRef.current) {
+                webSocketRef.current.close();
+            }
         };
     }, [url, onMessage]);
 
