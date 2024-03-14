47c9e86d8df4f019ad7d7f118d5c0f12fec17e82
LastCeri <125711498+LastCeri@users.noreply.github.com>
2024-03-14 23:33:53 +0300

    Page Update [75]
    Within the scope of the UI/UX Design Overhaul (Phase 8), I conducted Page Update [75]. I made aesthetic improvements to the user interface design and resolved issues including delays on the Group page. Additionally, I improved the listing of groups where the user is a member.

--- mogart-network/src/Pages/Groups/GroupsPage.tsx
+++ mogart-network/src/Pages/Groups/GroupsPage.tsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import Header from '../../MogartBase/ThemeParts/MainPart/Header/HeaderPart.tsx';
 import Navbar from '../../MogartBase/ThemeParts/MainPart/Navbar/Navbar.tsx';
-import { fetchGroups } from '../../MogartBase/Api/Api.tsx';
+import { API_URL } from '../../MogartBase/Api/Api.tsx';
 import CreateGroupPage from './SubPage/CreateGroups/CreateGroupsPage.tsx';
 import MyGroupsPage from './SubPage/MyGroups/MyGroups.tsx';
+import { useData } from '../../MogartBase/Context/DataContext.tsx';
 
 type Group = {
   GrpID: number;
@@ -13,45 +15,37 @@ type Group = {
   GrpImage: string;
 };
 
-const GroupItem: React.FC<{ group: Group }> = ({ group }) => {
-  return (
-    <div className="bg-white rounded-xl overflow-hidden shadow-lg transform transition-all hover:scale-105 duration-300">
-      <a href={"/Groups/" + group.GrpName.replace(/\s/g, "")}>
-        <img src={group.GrpImage} alt={`${group.GrpName}`} className="w-full h-32 sm:h-48 object-cover" />
-        <div className="px-6 py-4">
-          <div className="font-bold text-xl mb-2">{group.GrpName}</div>
-          <p className="text-gray-700 text-base">{group.GrpDesc}</p>
-        </div>
-        <div className="px-6 pt-4 pb-2">
-          <span className="inline-block bg-blue-200 rounded-full px-3 py-1 text-sm font-semibold text-blue-700 mr-2 mb-2">{group.GrpMembersCount} members</span>
-          <div className="flex space-x-2">
-          </div>
-        </div>
-      </a>
+const GroupItem: React.FC<{ group: Group }> = ({ group }) => (
+  <div className="bg-white rounded-xl overflow-hidden shadow-lg transform transition-all hover:scale-105 duration-300">
+    <a href={"/Groups/" + group.GrpName.replace(/\s/g, "")}>
+      <img src={group.GrpImage} alt={group.GrpName} className="w-full h-32 sm:h-48 object-cover" />
       <div className="px-6 py-4">
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          Join Group
-        </button>
+        <div className="font-bold text-xl mb-2">{group.GrpName}</div>
+        <p className="text-gray-700 text-base">{group.GrpDesc}</p>
       </div>
+      <div className="px-6 pt-4 pb-2">
+        <span className="inline-block bg-blue-200 rounded-full px-3 py-1 text-sm font-semibold text-blue-700 mr-2 mb-2">{group.GrpMembersCount} members</span>
+      </div>
+    </a>
+    <div className="px-6 py-4">
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Join Group
+      </button>
     </div>
-  );
-};
-
-
+  </div>
+);
 
 const GroupsPage = () => {
   const [activeTab, setActiveTab] = useState('all');
-  const createGroupPage = activeTab === 'create' ? <CreateGroupPage /> : null;
-  const MyGroupPage = activeTab === 'my' ? <MyGroupsPage /> : null;
-
   const [groups, setGroups] = useState<Group[]>([]);
+  const { isLoggedIn, isLoading, data,siteData } = useData();
 
   useEffect(() => {
     const fetchAndSetGroups = async () => {
       if (activeTab === 'all') {
         try {
-          const fetchedGroups: Group[] = await fetchGroups();
-          setGroups(fetchedGroups);
+          const response = await axios.get<Group[]>(`${API_URL}/GetGroups`);
+          setGroups(response.data);
         } catch (error) {
           console.error('Error fetching groups:', error);
         }
@@ -61,21 +55,19 @@ const GroupsPage = () => {
     fetchAndSetGroups();
   }, [activeTab]);
 
-  const getFilteredGroups = () => {
+  const renderGroupContent = () => {
     switch (activeTab) {
       case 'create':
-        return createGroupPage;
+        return <CreateGroupPage />;
       case 'my':
-        return MyGroupPage ;
+        return <MyGroupsPage />;
       case 'all':
-        return groups;
+        return groups.map(group => <GroupItem key={group.GrpID} group={group} />);
       default:
-        return groups;
+        return null;
     }
   };
 
-  const filteredGroups = getFilteredGroups();
-  
   return (
     <> 
       <Header />
@@ -86,24 +78,37 @@ const GroupsPage = () => {
             <div className="bg-white shadow">
               <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-24">
                 <div className="flex justify-between items-center py-6 md:space-x-10">
-                  <button
-                    onClick={() => setActiveTab('all')}
-                    className={`text-base font-medium ${activeTab === 'all' ? 'text-blue-600' : 'text-gray-500'} hover:text-gray-900`}
-                  >
-                    All Groups
-                  </button>
-                  <button
-                    onClick={() => setActiveTab('my')}
-                    className={`text-base font-medium ${activeTab === 'my' ? 'text-blue-600' : 'text-gray-500'} hover:text-gray-900`}
-                  >
-                    My Groups
-                  </button>
+                {isLoggedIn ? (
+                        <button
+                          onClick={() => setActiveTab('all')}
+                          className={`text-base font-medium ${activeTab === 'all' ? 'text-blue-600' : 'text-gray-500'} hover:text-gray-900`}
+                        >
+                          All Groups
+                        </button>
+                      ) : (
+                        <button
+                          onClick={() => setActiveTab('all')}
+                          className={`text-base font-medium ${activeTab === 'all' ? 'text-blue-600' : 'text-gray-500'} hover:text-gray-900`}
+                        >
+                          Discover Groups
+                        </button>
+                      )}
+                                      {isLoggedIn && (
+                        <button
+                          onClick={() => setActiveTab('my')}
+                          className={`text-base font-medium ${activeTab === 'my' ? 'text-blue-600' : 'text-gray-500'} hover:text-gray-900`}
+                        >
+                          My Groups
+                        </button>
+                      )}
+                   {isLoggedIn && (
                   <button
                     onClick={() => setActiveTab('create')}
                     className={`text-base font-medium ${activeTab === 'create' ? 'text-blue-600' : 'text-gray-500'} hover:text-gray-900`}
                   >
                     Create a Group
                   </button>
+                    )}
                   <div className="relative">
                     <input
                       type="search"
@@ -117,13 +122,7 @@ const GroupsPage = () => {
             </div>
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
               <div className={`grid ${activeTab === 'all' ? 'grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4' : 'grid-cols-1'} gap-4`}>
-              {Array.isArray(filteredGroups) ? (
-                filteredGroups.map(group => (
-                  <GroupItem key={group.GrpID} group={group} />
-                ))
-              ) : (
-                <>{filteredGroups}</>
-              )}
+                {renderGroupContent()}
             </div>
             </div>
           </div>
--- mogart-network/src/Pages/Groups/SubPage/MyGroups/MyGroups.tsx
+++ mogart-network/src/Pages/Groups/SubPage/MyGroups/MyGroups.tsx
@@ -1,4 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { API_URL } from '../../../../MogartBase/Api/Api';
+import { useData } from '../../../../MogartBase/Context/DataContext';
 
 interface Group {
   id: number;
@@ -17,9 +20,7 @@ const GroupItem: React.FC<{ group: Group }> = ({ group }) => {
         <p className="text-gray-700 text-base">{group.description}</p>
       </div>
       <div className="px-6 pt-4 pb-2">
-        <span className="inline-block bg-blue-200 rounded-full px-3 py-1 text-sm font-semibold text-blue-700 mr-2 mb-2">{group.tags.length} members</span>
-        <div className="flex space-x-2">
-        </div>
+        <span className="inline-block bg-blue-200 rounded-full px-3 py-1 text-sm font-semibold text-blue-700 mr-2 mb-2">{group.tags.length} tags</span>
       </div>
       <div className="px-6 py-4">
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
@@ -31,22 +32,26 @@ const GroupItem: React.FC<{ group: Group }> = ({ group }) => {
 };
 
 const MyGroupsPage = () => {
-  const [myGroups, setMyGroups] = useState<Group[]>([
-    {
-      id: 1,
-      name: 'Group 1',
-      description: 'Description for Group 1',
-      tags: ['tag1', 'tag2'],
-      logo: 'https://example.com/logo1.jpg',
-    },
-    {
-      id: 2,
-      name: 'Group 2',
-      description: 'Description for Group 2',
-      tags: ['tag3', 'tag4'],
-      logo: 'https://example.com/logo2.jpg',
-    },
-  ]);
+  const [myGroups, setMyGroups] = useState<Group[]>([]);
+  const { isLoggedIn, isLoading, data,siteData } = useData();
+
+  useEffect(() => {
+    const fetchMyGroups = async () => {
+      try {
+        const response = await axios.get(`${API_URL}/GetGroups/${data.UserName}`);
+        setMyGroups(response.data); 
+      } catch (error) {
+        console.error('Error fetching my groups:', error);
+      }
+    };
+
+    fetchMyGroups();
+  }, []); 
+  if (!isLoggedIn) return <p className="text-center text-black-600 font-semibold ml-4">No group information can be obtained without user login.</p>;
+  if (isLoading) return  <div className="flex justify-center items-center h-screen">
+  <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-purple-500"></div>
+  <p className="text-lg text-purple-600 font-semibold ml-4">Loading...</p>
+</div>;
 
   return (
     <main className="flex p-4 bg-gray-100 h-screen">
@@ -54,7 +59,7 @@ const MyGroupsPage = () => {
         <h1 className="text-3xl font-bold text-gray-700 mb-6">My Groups</h1>
 
         {myGroups.length === 0 ? (
-          <p className="text-gray-600">You haven't created any groups yet.</p>
+          <p className="text-gray-600">You haven't joined any groups yet.</p>
         ) : (
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
             {myGroups.map((group) => (
