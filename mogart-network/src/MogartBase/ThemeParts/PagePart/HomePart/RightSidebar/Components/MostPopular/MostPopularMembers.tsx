61393e65348da4d1ca7ee178944960903fc0fb1b
LastCeri <125711498+LastCeri@users.noreply.github.com>
2024-03-22 02:59:25 +0300

    Part Update [44]
    Within the scope of the UI/UX Design Overhaul (Phase 8), Part Update [44] has been implemented. As part of Part Update [44], I created a design for MostPopularMembers where users can track popularity trends and understand why they are popular.

--- mogart-network/src/MogartBase/ThemeParts/PagePart/HomePart/RightSidebar/Components/MostPopular/MostPopularMembers.tsx
+++ mogart-network/src/MogartBase/ThemeParts/PagePart/HomePart/RightSidebar/Components/MostPopular/MostPopularMembers.tsx
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { API_URL } from '../../../../../../Api/Api';
 import axios from 'axios';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faArrowDown, faArrowUp, faShieldAlt} from '@fortawesome/free-solid-svg-icons';
 
 
 const MostPopularMember = () => {
-  const [mostPopularMembers, setMostPopularMembers] = useState<{ Mpid: number; MpAvatar: string; MpUsername: string; MpUserID: string }[]>([]);
+  const [mostPopularMembers, setMostPopularMembers] = useState<{ Mpid: number; MpAvatar: string; MpUsername: string; MpUserID: string, MpPoints: string ,MpTrend: string }[]>([]);
 
   useEffect(() => {
     axios.get(`${API_URL}/GetMPM`) 
@@ -22,17 +24,34 @@ const MostPopularMember = () => {
     <div className="mb-6 bg-white p-4 rounded-lg shadow-lg">
       <h5 className="text-lg font-semibold mb-4">Most Popular Members</h5>
       <div className="bg-white p-4 rounded-lg">
-      {mostPopularMembers.map((member) => (
-        <div key={member.Mpid} className="py-2 hover:bg-gray-300 rounded-md transition duration-200">
-          <Link to={`Profile/${member.MpUsername}`} className="flex items-center space-x-3">
-            <img className="h-6 w-6 rounded-full" src={member.MpAvatar} alt={member.MpUsername} />
-            <span className="text-sm font-medium">{member.MpUsername}</span>
-          </Link>
-        </div>
-      ))}
+        {mostPopularMembers.map((member) => (
+          <div key={member.Mpid} className="py-2 hover:bg-gray-100 rounded-md transition duration-200 ease-in-out flex justify-between items-center">
+            <Link to={`Profile/${member.MpUsername}`} className="flex items-center space-x-3">
+              <img className="h-10 w-10 rounded-full object-cover border-2 border-gray-300" src={member.MpAvatar} alt={member.MpUsername} />
+              <div>
+                <span className="text-sm font-medium text-gray-800">{member.MpUsername}</span>
+              </div>
+            </Link>
+            <div className="flex items-center space-x-1">
+            <span className="text-sm font-medium text-gray-600 mx-6 hover:text-gray-800 transition duration-150 ease-in-out transform hover:scale-105 shadow-md">
+              {member.MpPoints} Points
+            </span>
+
+            {member.MpTrend === 'up' ? (
+                <FontAwesomeIcon icon={faArrowUp} className="text-green-500" />
+              ) : member.MpTrend === 'down' ? (
+                <FontAwesomeIcon icon={faArrowDown} className="text-red-500" />
+              ) : (
+                <FontAwesomeIcon icon={faShieldAlt} className="text-blue-500" />
+              )
+            }
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
 };
+
 
 export default MostPopularMember;
