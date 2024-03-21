429107ef4e5b6da1281f9dbcb83c0c44835e75cc
LastCeri <125711498+LastCeri@users.noreply.github.com>
2024-03-22 02:51:08 +0300

    Part Update [42]
    Within the scope of the UI/UX Design Overhaul (Phase 8), Part Update [42] has been implemented. As part of Part Update [42], I made design improvements to the HeaderPart.

--- mogart-network/src/MogartBase/ThemeParts/MainPart/Header/HeaderPart.tsx
+++ mogart-network/src/MogartBase/ThemeParts/MainPart/Header/HeaderPart.tsx
@@ -1,6 +1,8 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { useData } from '../../../../MogartBase/Context/DataContext';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGear,faEllipsisV, faUser} from '@fortawesome/free-solid-svg-icons';
 
 export default function Header() {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
@@ -33,24 +35,31 @@ export default function Header() {
     <>
       <header className="fixed top-0 left-16 right-0 bg-white shadow-md z-10">
         <div className="container mx-auto px-6 py-3 flex justify-between items-center">
-          <div className="flex items-center">
-            <img src={siteLogoURL} alt="Mogart Network Logo" className="h-8 w-8 mr-2"/>
-            <span className="font-bold text-xl text-gray-800">Mogart Network</span>
+          <Link to="/" className="">
+          <div className="flex items-center w-auto">
+              <img src={siteLogoURL} alt="Mogart Network Logo" className="h-8 w-8 mr-2"/>
+              <span className="font-bold text-xl text-gray-800">Mogart Network</span>
           </div>
-          
+          </Link>
           {isLoggedIn ? (
             <div className="flex items-center relative" ref={dropdownRef}>
+              <Link to="/Profile" className="">
               <img 
                 src={profileImageURL}
                 alt="User Profile" 
-                className="h-10 w-10 rounded-full cursor-pointer"
-                onClick={toggleDropdown}
+                className="h-12 w-12 rounded-full cursor-pointer object-cover mr-2"
               />
+              </Link>
+              <button className="px-4 py-2 rounded focus:outline-none" onClick={toggleDropdown}>
+                <FontAwesomeIcon icon={faEllipsisV} className="text-gray-700 hover:text-gray-900 transition duration-150 ease-in-out" />
+              </button>
               {isLoggedIn && isDropdownOpen && (
-                <div className="absolute right-0 mt-36 py-2 w-48 bg-white rounded-md shadow-xl z-20">
-                  <Link to="/Settings" className="block px-4 py-2 text-sm text-gray-700 hover:bg-blue-500 hover:text-white">Settings</Link>
-                  <Link to="/Profile" className="block px-4 py-2 text-sm text-gray-700 hover:bg-blue-500 hover:text-white">Profile</Link>
-                </div>
+               <div className="absolute right-0 mt-48 py-2 w-48 bg-white rounded-md shadow-xl z-20 border border-gray-200">
+               <span className="block font-bold text-md text-gray-800 px-4 py-2">Hi! {data.Displayname}</span>
+               <hr className="my-2 border-t border-gray-200" />
+               <Link to="/Profile" className="block px-4 py-2 text-sm text-gray-700 hover:bg-blue-500 hover:text-white transition duration-150 ease-in-out"> <FontAwesomeIcon icon={faUser} className="mr-2" /> Profile</Link>
+               <Link to="/Settings" className="block px-4 py-2 text-sm text-gray-700 hover:bg-blue-500 hover:text-white transition duration-150 ease-in-out"> <FontAwesomeIcon icon={faGear} className="mr-2" /> Settings</Link>
+             </div>
               )}
             </div>
           ) : null}
