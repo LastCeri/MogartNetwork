b50cfd6515e3baa99759c68e3fa9edc90057ba8d
LastCeri <125711498+LastCeri@users.noreply.github.com>
2024-03-22 03:09:50 +0300

    Part Update [45]
    Within the scope of the UI/UX Design Overhaul (Phase 8), Part Update [45] has been implemented. As part of Part Update [45], I made design updates to Main before integrating it with the content management system (CMS). It hadn't received updates for a very long time.

--- mogart-network/src/MogartBase/ThemeParts/PagePart/HomePart/Main/Main.tsx
+++ mogart-network/src/MogartBase/ThemeParts/PagePart/HomePart/Main/Main.tsx
@@ -15,9 +15,8 @@ interface PostType {
 
 function Post({ Author, Avatar,GlobalId, Content: PostContent, Date: PostDate }: PostType): React.JSX.Element {
   const [showCommentInput, setShowCommentInput] = useState(false);
-
+  const {siteData} = useData();
   const handleLike = () => {
-    console.log("Like button pressed.");
   };
 
   const handleComment = () => {
@@ -25,7 +24,6 @@ function Post({ Author, Avatar,GlobalId, Content: PostContent, Date: PostDate }:
   };
 
   const handleShare = () => {
-    console.log("Share button pressed.");
   };
 
   return (
@@ -33,7 +31,7 @@ function Post({ Author, Avatar,GlobalId, Content: PostContent, Date: PostDate }:
       <div className="flex items-center justify-between mb-3">
       <Link to={`/posts/${GlobalId}`} key={GlobalId} style={{ textDecoration: 'none' }}>
         <div className="flex items-center">
-          <img className="h-8 w-8 rounded-full mr-2" src={Avatar || 'default-avatar-url'} alt={`${Author}'s avatar`} />
+          <img className="h-8 w-8 rounded-full mr-2 object-cover" src={Avatar || siteData?.SiteDefaultProfileImageURL} alt={`${Author}'s avatar`} />
           <div>
             <div className="font-medium">{Author}</div>
             <div className="text-xs text-gray-500">{PostDate}</div>
@@ -53,7 +51,7 @@ function Post({ Author, Avatar,GlobalId, Content: PostContent, Date: PostDate }:
           <FontAwesomeIcon icon={faThumbsUp} className="mr-1" /> Like
         </button>
         <Link to={`/posts/${GlobalId}`} key={GlobalId} style={{ textDecoration: 'none' }}>
-          <button type="button" className="text-gray-500 hover:text-green-600 focus:outline-none mx-2">
+          <button type="button" onClick={handleComment} className="text-gray-500 hover:text-green-600 focus:outline-none mx-2">
             <FontAwesomeIcon icon={faComment} className="mr-1" /> Comment
           </button>
         </Link>
@@ -78,7 +76,7 @@ function Post({ Author, Avatar,GlobalId, Content: PostContent, Date: PostDate }:
 }
 
 function MainContent() {
-  const { isLoggedIn, data, isLoading } = useData();
+  const { isLoggedIn, data, isLoading,siteData } = useData();
   const [postContent, setPostContent] = useState('');
   const postsFromApi = useFetchMogartPosts();
   const [posts, setPosts] = useState<PostType[]>([]);
@@ -88,33 +86,27 @@ function MainContent() {
         const newPost = {
             Content: postContent,
         };
-
         try {
             const response = await createPost(newPost);
             if (response && response.status === "Ok") {
                 console.log('Post successfully created!');
-                // Additional actions after successful post creation can be added here
-                // For example, you can update the user interface
             } else {
-                // We couldn't get the expected response from the API, show an error message to the user
                 console.error('Post creation failed, please try again later.');
             }
         } catch (error) {
-            // An error occurred during the API call, log the error message
             console.error('Error while posting:', error);
-            // Additional actions in case of an error can be added here
-            // For example, you can display an error message
         }
 
-        // Whether the post operation is successful or not, clear the post content
         setPostContent('');
     } else {
-        // If postContent is empty, you can show a warning to the user
         console.warn('Post content cannot be empty.');
     }
 };
 
   useEffect(() => {
+    if (isLoading) {
+      return;
+    }
     setPosts(postsFromApi);
   }, [postsFromApi]);
   
@@ -122,30 +114,31 @@ function MainContent() {
   return (
     <main className="w-1/2 sm:w-3/6 top-20 mr-16" style={{ marginRight: '20px' }}> 
       {isLoggedIn && (
-        <div className="bg-white p-4 rounded-lg shadow mb-4 flex items-center space-x-4">
-          <img className="h-10 w-10 rounded-full" src={data.ProfileImage} alt="User Avatar" />
-          <input
-            className="form-input p-2 w-3/4 border rounded-md"
-            type="text"
-            placeholder="What's on your mind?"
-            value={postContent}
-            onChange={(e) => setPostContent(e.target.value)}
-          />
-          <button
-            onClick={handlePostButtonClick}
-            className="h-8 w-20 bg-blue-500 rounded-full flex items-center justify-center text-white hover:bg-blue-600 transition duration-300 ease-in-out"
-          >
-            <FontAwesomeIcon icon={faPaperPlane} className="h-5 w-5" />
-          </button>
-        </div>
+        <div className="bg-white p-4 rounded-lg shadow-md mt-4 mb-4 flex items-center space-x-4">
+        <img className="h-12 w-12 rounded-full object-cover" src={data?.ProfileImage || siteData?.SiteDefaultProfileImageURL} alt="User Avatar" />
+        <input
+          className="form-input flex-1 py-2 px-4 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500 transition ease-in-out duration-150"
+          type="text"
+          placeholder="What's on your mind?"
+          value={postContent}
+          onChange={(e) => setPostContent(e.target.value)}
+        />
+        <button
+          onClick={handlePostButtonClick}
+          className="px-4 py-2 bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 rounded-full flex items-center justify-center text-white transition duration-300 ease-in-out"
+        >
+          <FontAwesomeIcon icon={faPaperPlane} className="h-5 w-5" />
+        </button>
+      </div>
       )}
       
-      <div className="bg-white p-4 rounded-lg shadow mb-4">
-        <ul className="flex items-center space-x-4">
-          <li className="text-blue-500 cursor-pointer">All Members</li>
-          <li className="cursor-pointer">My Groups</li>
-          <li className="cursor-pointer">My Favorites</li>
-          <li className="cursor-pointer">Mentions</li>
+      <div className="bg-white p-6 rounded-lg shadow-lg mb-6">
+        <ul className="flex items-center space-x-6">
+          <li className="text-blue-500 cursor-pointer font-semibold hover:text-blue-600 transition duration-150 ease-in-out">Global Members</li>
+          <li className="cursor-pointer hover:text-gray-700 transition duration-150 ease-in-out">Local</li>
+          <li className="cursor-pointer hover:text-gray-700 transition duration-150 ease-in-out">Latest</li>
+          <li className="cursor-pointer hover:text-gray-700 transition duration-150 ease-in-out">Follows</li>
+          <li className="cursor-pointer hover:text-gray-700 transition duration-150 ease-in-out">Mentions</li>
         </ul>
       </div>
       <div className="space-y-4">
