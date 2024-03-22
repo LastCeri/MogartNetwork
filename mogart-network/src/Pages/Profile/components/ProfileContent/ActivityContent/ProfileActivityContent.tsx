d45939871df853102bf03277ad48154e0241eb9b
LastCeri <125711498+LastCeri@users.noreply.github.com>
2024-03-22 21:00:18 +0300

    Part Update [47]
    Within the scope of the UI/UX Design Overhaul (Phase 8), Part Update [47] has been implemented. Under Part Update [47], I made design updates for the ProfileActivityContent. Users can now create, edit, and delete new, pending, and past activities, providing them with a comprehensive structure.

--- mogart-network/src/Pages/Profile/components/ProfileContent/ActivityContent/ProfileActivityContent.tsx
+++ mogart-network/src/Pages/Profile/components/ProfileContent/ActivityContent/ProfileActivityContent.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCalendarAlt, faUser, faStar } from '@fortawesome/free-solid-svg-icons';
+import { faCalendarAlt, faUser, faStar, faEnvelope, faClock, faPlus } from '@fortawesome/free-solid-svg-icons';
 import { UserData } from '../../../Profile';
+import PendingActivity from './Modals/PendingActivity';
+import PastActivity from './Modals/PastActivity';
+import CreateActivity from './Modals/CreateActivity';
 
 const dummyUserData = {
   Activity: [
@@ -16,35 +19,44 @@ interface ProfileActivityContentProps {
 }
 
 const ProfileActivityContent: React.FC<ProfileActivityContentProps> = ({ userData }) => {
-  const activities = dummyUserData?.Activity || [];
+  const [activeModal, setActiveModal] = useState('');
+  const invitations = dummyUserData?.Activity || [];
+
+  const handleCreateInvitation = (invitation:any) => {
+    console.log(invitation); 
+    setActiveModal(''); 
+  };
+
+  let contentComponent;
+  switch (activeModal) {
+    case 'pending':
+      contentComponent = <PendingActivity userData={userData} isOpen={true} onClose={() => setActiveModal('')} onSubmit={handleCreateInvitation} />;
+      break;
+      case 'past':
+      contentComponent = <PastActivity userData={userData} isOpen={true} onClose={() => setActiveModal('')} onSubmit={handleCreateInvitation} />;
+      break;
+      case 'create':
+      contentComponent = <CreateActivity userData={userData} isOpen={true} onClose={() => setActiveModal('')} onSubmit={handleCreateInvitation} />;
+      break;
+    default:
+      contentComponent = <PendingActivity userData={userData} isOpen={true} onClose={() => setActiveModal('')} onSubmit={handleCreateInvitation} />;
+  }
+
 
   return (
     <main className="flex-1 p-6 overflow-auto">
-      {activities.length === 0 ? (
-        <div className="flex items-center justify-center h-full">
-          <p className="text-gray-500 text-lg">No activities available.</p>
-        </div>
-      ) : (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-          {activities.map((activity, index) => (
-            <div key={index} className="bg-white rounded-lg shadow hover:shadow-xl transition-shadow duration-300 ease-in-out p-4">
-              <h3 className="text-lg font-semibold">{activity.Activity}</h3>
-              <p className="text-sm text-gray-500 my-2">
-                <FontAwesomeIcon icon={faUser} className="mr-2" />
-                {activity.Name}
-              </p>
-              <p className="text-sm text-gray-500 my-2">
-                <FontAwesomeIcon icon={faCalendarAlt} className="mr-2" />
-                {activity.Date}
-              </p>
-              <p className="text-sm text-gray-500 my-2">
-                <FontAwesomeIcon icon={faStar} className="mr-2" />
-                {activity.Point} Points
-              </p>
-            </div>
-          ))}
-        </div>
-      )}
+     <div className="flex justify-center items-center space-x-4">
+     <button onClick={() => setActiveModal('pending')} className="mb-4 px-4 py-2 rounded text-purple-600 border border-purple-500 hover:bg-purple-700 hover:text-white transition ease-in-out duration-150 shadow-md hover:shadow-lg">
+     <FontAwesomeIcon icon={faEnvelope} className="mr-2" /> Pending Activity
+      </button>
+      <button onClick={() => setActiveModal('past')} className="mb-4 px-4 py-2 rounded text-green-600 border border-green-600 hover:bg-green-700 hover:text-white transition ease-in-out duration-150 shadow-md hover:shadow-lg">
+      <FontAwesomeIcon icon={faClock} className="mr-2" /> Past Activity
+      </button>
+      <button onClick={() => setActiveModal('create')} className="mb-4 px-4 py-2 rounded text-blue-500 border border-blue-500 hover:bg-blue-500 hover:text-white transition ease-in-out duration-150 shadow-md hover:shadow-lg">
+      <FontAwesomeIcon icon={faPlus} className="mr-2" />  Create Activity
+      </button>
+    </div>
+      {contentComponent}
     </main>
   );
 };
