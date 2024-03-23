d825de3084b986baf17e276b1c78594e67a146be
LastCeri <125711498+LastCeri@users.noreply.github.com>
2024-03-23 15:50:30 +0300

    Add Pages [20]
    Within the scope of the UI/UX Design Overhaul (Phase 8), Add Pages [20] has been implemented. Under Add Pages [20], I added the CommunicationPage, where users can manage all their incoming requests by accepting, rejecting, or putting them on hold.

---
+++ mogart-network/src/Pages/Communication/CommunicationPage.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import Header from '../../MogartBase/ThemeParts/MainPart/Header/HeaderPart';
+import Navbar from '../../MogartBase/ThemeParts/MainPart/Navbar/Navbar';
+import FriendRequests from './components/FriendRequests';
+import EventRequests from './components/EventRequests';
+import GroupRequests from './components/GroupRequest';
+import MessageRequests from './components/MessageRequests';
+
+const CommunicationPage = () => {
+  const [activeModule, setActiveModule] = useState('friendRequests');
+
+  return (
+    <>
+      <Header />
+      <Navbar />
+      <div className="flex flex-col md:flex-row h-screen overflow-auto mt-24">
+        <div className="md:w-1/4 lg:w-1/5 bg-gray-100 text-gray-800 overflow-auto ml-20 shadow-lg">
+        <div className="p-5">
+            <h2 className="text-2xl font-bold mb-5">Communication Requests</h2>
+            <div className="space-y-3">
+            <button onClick={() => setActiveModule('friendRequests')} className={`w-full text-left font-medium ${activeModule === 'friendRequests' ? 'bg-blue-500 text-white' : 'bg-white text-gray-800'} py-3 px-5 rounded-lg shadow transition duration-300 ease-in-out hover:shadow-md`}>
+                Friend Requests
+            </button>
+            <button onClick={() => setActiveModule('eventRequests')} className={`w-full text-left font-medium ${activeModule === 'eventRequests' ? 'bg-blue-500 text-white' : 'bg-white text-gray-800'} py-3 px-5 rounded-lg shadow transition duration-300 ease-in-out hover:shadow-md`}>
+                Event Requests
+            </button>
+            <button onClick={() => setActiveModule('groupRequests')} className={`w-full text-left font-medium ${activeModule === 'groupRequests' ? 'bg-blue-500 text-white' : 'bg-white text-gray-800'} py-3 px-5 rounded-lg shadow transition duration-300 ease-in-out hover:shadow-md`}>
+                Group Requests
+            </button>
+            <button onClick={() => setActiveModule('messageRequests')} className={`w-full text-left font-medium ${activeModule === 'messageRequests' ? 'bg-blue-500 text-white' : 'bg-white text-gray-800'} py-3 px-5 rounded-lg shadow transition duration-300 ease-in-out hover:shadow-md`}>
+                Message Requests
+            </button>
+            </div>
+        </div>
+        </div>
+
+        <div className="flex-1 flex flex-col">
+          <div className="bg-gray-100 p-4 shadow-md">
+            <h3 className="text-lg font-semibold">Requests Details</h3>
+          </div>
+          
+          <div className="flex-1 overflow-y-auto p-4">
+            {activeModule === 'friendRequests' && <FriendRequests />}
+            {activeModule === 'eventRequests' && <EventRequests />}
+            {activeModule === 'groupRequests' && <GroupRequests />}
+            {activeModule === 'messageRequests' && <MessageRequests />}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CommunicationPage;
