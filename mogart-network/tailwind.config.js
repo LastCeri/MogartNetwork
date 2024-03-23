3519e3251f04387f552c4b4ec6d7e25ec0b37e1a
LastCeri <125711498+LastCeri@users.noreply.github.com>
2024-03-23 15:28:28 +0300

    Update package.json & package-lock.json & tailwind.config.js
    I updated the dependencies for the project's new dependencies.

--- mogart-network/tailwind.config.js
+++ mogart-network/tailwind.config.js
@@ -5,7 +5,22 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        pulseGreenBorder: {
+          '0%, 100%': { borderColor: 'transparent' },
+          '50%': { borderColor: 'rgba(5, 150, 105, 0.75)' }, 
+        },
+        voiceActivity: {
+          '0%, 100%': { opacity: '0.5' },
+          '50%': { opacity: '0.75' },
+        },
+      },
+      animation: {
+        pulseGreenBorder: 'pulseGreenBorder 5s infinite',
+        voiceActivity: 'voiceActivity 10.5s infinite ease-in-out',
+      },
+    },
   },
   plugins: [],
 }
