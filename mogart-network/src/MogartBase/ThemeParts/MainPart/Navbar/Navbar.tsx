d4d860d6c09050a2fa82898e1b70af4b1605cba0
LastCeri <125711498+LastCeri@users.noreply.github.com>
2024-03-23 16:26:47 +0300

    Part Update [53]
    Within the scope of the UI/UX Design Overhaul (Phase 8), Part Update [53] has been implemented. This update includes the addition of the new CommunicationPage to the Navbar.

--- mogart-network/src/MogartBase/ThemeParts/MainPart/Navbar/Navbar.tsx
+++ mogart-network/src/MogartBase/ThemeParts/MainPart/Navbar/Navbar.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link,useNavigate } from 'react-router-dom';
-import { faHome, faSearch, faBell, faEnvelope, faCog, faPowerOff, faPeopleGroup, faMugHot } from '@fortawesome/free-solid-svg-icons';
+import { faHome, faSearch, faBell, faEnvelope, faCog, faPowerOff, faPeopleGroup, faMugHot, faMessage } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useData } from '../../../../MogartBase/Context/DataContext';
 import { logout } from '../../../Api/Api';
@@ -25,9 +25,10 @@ export default function Navbar() {
     { icon: faHome, alt: 'Home', to: '/', style: { color: "#6684b3" }},
     { icon: faSearch, alt: 'Search', to: '/Search', style: { color: "#545e75" }},
     { icon: faBell, alt: 'Notifications', to: createUserDependentUrl('/Notifications', '/Notifications'), style: { color: "#545e75" }},
+    { icon: faEnvelope, alt: 'Communication', to: '/Communication', style: { color: "#545e75" }},
+    { icon: faMessage, alt: 'Messages', to: '/Messages', style: { color: "#545e75" }},
     { icon: faPeopleGroup, alt: 'Groups', to: '/Groups', style: { color: "#545e75" }},
     { icon: faMugHot, alt: 'Activity', to: createUserDependentUrl('/Activity', '/Activity'), style: { color: "#545e75" }},
-    { icon: faEnvelope, alt: 'Messages', to: '/Messages', style: { color: "#545e75" }},
     { icon: faCog, alt: 'Settings', to: '/Settings', style: { color: "#545e75" }},
   ];
 
