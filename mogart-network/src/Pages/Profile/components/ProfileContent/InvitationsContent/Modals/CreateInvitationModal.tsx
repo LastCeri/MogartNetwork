8491029707c5c4c69cb7e227ea03d0da3c083458
LastCeri <125711498+LastCeri@users.noreply.github.com>
2024-03-26 22:48:15 +0300

    Part Update [61] (Added Invations)
    Within the scope of the Invitations and invitation keys integration, Part Update [61] has been implemented. In this update, I revised the CreateInvitationModal component. Users can now create invitations for events, meetings, webinars, or groups that they want to organize or participate in, as well as other types.

--- mogart-network/src/Pages/Profile/components/ProfileContent/InvitationsContent/Modals/CreateInvitationModal.tsx
+++ mogart-network/src/Pages/Profile/components/ProfileContent/InvitationsContent/Modals/CreateInvitationModal.tsx
@@ -1,5 +1,7 @@
 
 import React, { useState } from 'react';
+import { CreateEventInvation } from '../../../../../../MogartBase/Api/Api';
+import { useData } from '../../../../../../MogartBase/Context/DataContext';
 
 interface CreateInvitationModalProps {
     isOpen: boolean;
@@ -21,8 +23,27 @@ const CreateInvitationModal: React.FC<CreateInvitationModalProps> = ({ isOpen, o
     const [invitationType, setInvitationType] = useState('');
     const [accessType, setAccessType] = useState('');
     const [entryFee, setEntryFee] = useState('');
+    const [popup, setPopup] = useState({ visible: false, message: '' });
+    const { data, userAuthID} = useData();
+
 
   if (!isOpen) return null;
+
+  const handleInvitations = async (subject:string, validUntil:string, invitationType:string, accessType:string, entryFee:string) => {
+    await CreateEvent(subject, validUntil, invitationType, accessType, entryFee);
+    setPopup({ visible: true, message: 'CreateEvent' });
+    setTimeout(() => setPopup({ visible: false, message: '' }), 3000);
+  };
+
+{/* The merkle&witness operation will come here along with the Contract along with o1js. */}
+  const CreateEvent = async (subject:string, validUntil:string, invitationType:string, accessType:string, entryFee:string) => {
+    const response = await CreateEventInvation({ UserID:userAuthID, Subject:subject, ValidUntil:validUntil, InvitationType:invitationType, AccessType:accessType, EntryFee:entryFee, WalletAdress: data.WalletAddress, TransactionHash:"" });
+    console.log(response);
+    setPopup({ visible: true, message: 'CreateEvent' });
+    setTimeout(() => setPopup({ visible: false, message: '' }), 3000);
+    onClose();
+  };
+{/* ------------------------------------------------------------------------------------ */}
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-60 flex justify-center items-center">
@@ -33,28 +54,30 @@ const CreateInvitationModal: React.FC<CreateInvitationModalProps> = ({ isOpen, o
           </h2>
           <p className="text-gray-600">Fill in the details below to create a new invitation.</p>
         </div>
-
+  
         <form onSubmit={(e) => {
           e.preventDefault();
-          onSubmit({ subject, validUntil, invitationType });
+          handleInvitations(subject, validUntil, invitationType, accessType, entryFee);
         }}>
           <div className="mb-6">
             <label htmlFor="subject" className="block text-md font-medium text-gray-700">Subject</label>
             <input type="text" id="subject" value={subject} onChange={(e) => setSubject(e.target.value)}
-                   className="mt-2 p-3 w-full border border-gray-300 rounded-lg shadow-sm placeholder-gray-500 focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500"/>
+                   className="mt-2 p-3 w-full border border-gray-300 rounded-lg shadow-sm placeholder-gray-500 focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500"
+                   required />
           </div>
           <div className="mb-6">
             <label htmlFor="validUntil" className="block text-md font-medium text-gray-700">Valid Until</label>
             <input type="date" id="validUntil" value={validUntil} onChange={(e) => setValidUntil(e.target.value)}
-                   className="mt-2 p-3 w-full border border-gray-300 rounded-lg shadow-sm placeholder-gray-500 focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500"/>
+                   className="mt-2 p-3 w-full border border-gray-300 rounded-lg shadow-sm placeholder-gray-500 focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500"
+                   required />
           </div>
           <div className="mb-6">
             <label htmlFor="invitationType" className="block text-md font-medium text-gray-700">Invitation Type</label>
             <select id="invitationType" value={invitationType} onChange={(e) => setInvitationType(e.target.value)}
-                    className="mt-2 p-3 w-full border border-gray-300 rounded-lg shadow-sm placeholder-gray-500 focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500">
+                    className="mt-2 p-3 w-full border border-gray-300 rounded-lg shadow-sm placeholder-gray-500 focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500"
+                    required>
               <option value="">Select Type</option>
-              <option value="Webinar">Friends</option>
-              <option value="Webinar">Groups</option>
+              <option value="Groups">Groups</option>
               <option value="Event">Event</option>
               <option value="Meeting">Meeting</option>
               <option value="Webinar">Webinar</option>
@@ -64,7 +87,8 @@ const CreateInvitationModal: React.FC<CreateInvitationModalProps> = ({ isOpen, o
           <div className="mb-6">
             <label htmlFor="accessType" className="block text-md font-medium text-gray-700">Access Type</label>
             <select id="accessType" value={accessType} onChange={(e) => setAccessType(e.target.value)}
-                    className="mt-2 p-3 w-full border border-gray-300 rounded-lg shadow-sm">
+                    className="mt-2 p-3 w-full border border-gray-300 rounded-lg shadow-sm"
+                    required>
               <option value="">Select Access Type</option>
               <option value="Public">Public</option>
               <option value="Limited">Limited</option>
@@ -93,6 +117,15 @@ const CreateInvitationModal: React.FC<CreateInvitationModalProps> = ({ isOpen, o
             </button>
           </div>
         </form>
+
+        {popup.visible && (
+            <div className="fixed bottom-0 inset-x-0 pb-4 flex justify-center items-center">
+              <div className="bg-white rounded-lg px-6 py-4 shadow-xl border border-gray-200">
+                <p className="text-sm text-gray-800">{popup.message}</p>
+              </div>
+            </div>
+          )}
+
       </div>
     </div>
   );
