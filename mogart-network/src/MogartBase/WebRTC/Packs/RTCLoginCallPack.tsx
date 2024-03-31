8766856237dd456ef91e67343fad8478a1240c12
LastCeri <125711498+LastCeri@users.noreply.github.com>
2024-03-31 23:17:21 +0300

    Voice Chat Update [6] (Multi RTC Handler System)
    Within the scope of the Voice Chat Technology Enhancement Integration, Voice Chat Update [6] has been implemented. I have made additions to the structure necessary for packet communication for the system to log in with the voice chat server.

---
+++ mogart-network/src/MogartBase/WebRTC/Packs/RTCLoginCallPack.tsx
@@ -0,0 +1,9 @@
+export async function RTCLoginPack(userAuthID: string, userData: any) {
+  const RTCLoginPacket = JSON.stringify({
+      username: userData.UserName,
+      userauthid: userAuthID,
+      type: 2
+  });
+
+  return RTCLoginPacket;
+}
