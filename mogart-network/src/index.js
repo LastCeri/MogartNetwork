a08ba05a6dd24f5667220a8cbfd4bf072822ebb8
LastCeri <125711498+LastCeri@users.noreply.github.com>
2024-04-08 23:37:31 +0300

    Update Context VoiceCallProvider
    I wrapped the VoiceCallProvider context into the entire project

--- mogart-network/src/index.js
+++ mogart-network/src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App.tsx';
 import reportWebVitals from './reportWebVitals';
 import { DataProvider } from './MogartBase/Context/DataContext';
-
+import { VoiceCallProvider } from './MogartBase/Context/VoiceCallProvider';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
   <DataProvider>
-    <App />
+      <VoiceCallProvider> 
+        <App />
+      </VoiceCallProvider>
   </DataProvider>
 </React.StrictMode>
 );
