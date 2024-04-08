954ad245071c34d3c1ca4b315a3d7b2de939d34b
LastCeri <125711498+LastCeri@users.noreply.github.com>
2024-04-08 20:40:01 +0300

    Page Update [114]
    Within the scope of the UI/UX Design Overhaul (Phase 9), Page Update [114] (CategoryDetails) has been implemented. In this update, I made design enhancements to the CategoryDetails page and implemented important changes to the API and interfaces.

--- mogart-network/src/MogartBase/Details/CategoryDetails/CategoryDetails.tsx
+++ mogart-network/src/MogartBase/Details/CategoryDetails/CategoryDetails.tsx
@@ -7,9 +7,10 @@ import { API_URL } from '../../Api/Api';
 import { useData } from '../../../MogartBase/Context/DataContext.tsx';
 
 interface ContentItem {
-  id: string;
+  ID: string;
   type: string;
   content: string;
+  desc: string;
   Category: string;
   tags: string[];
   author: string;
@@ -24,6 +25,7 @@ interface ApiResponseItem {
   PostName: string;
   PostTitle: string;
   PostAuthor: string;
+  Desc: string;
   PostAuthorAvatar: string;
   PostCategory: string;
   PostImage:string;
@@ -56,13 +58,14 @@ const CategoryDetails: React.FC = () => {
       try {
         const response = await axios.get<ApiResponseItem[]>(`${API_URL}/GetDetailCategory/${catname}`);
         const filteredData = response.data.filter(item => item.PostID || item.PostID);
-        const formattedContent = filteredData.map(({ PostID = '', PostType, PostContent, Category, PostTags, PostAuthor, PostDate, PostAuthorAvatar, PostImage }) => ({
-          id: (PostID).toString(),
+        const formattedContent = filteredData.map(({ PostID, PostType,Desc, PostContent, Category, PostTags, PostAuthor, PostDate, PostAuthorAvatar, PostImage }) => ({
+          ID: (PostID).toString(),
           type: PostType,
           content: PostContent,
           Category,
           tags: PostTags ? PostTags.split(',') : [],
           author: PostAuthor,
+          desc:Desc,
           date: PostDate,
           authorAvatar: PostAuthorAvatar,
           image: PostImage
@@ -109,11 +112,11 @@ interface CategoryButtonsProps {
 }
 
 const CategoryButtons: React.FC<CategoryButtonsProps> = ({ categories, setSelectedCategory, selectedCategory }) => (
-  <div className="flex justify-center items-center max-w-3xl mx-auto mt-20 my-8 bg-gray-100 shadow-lg rounded-lg p-4">
-    <div className="flex space-x-2 overflow-x-auto">
+  <div className="flex justify-center items-center max-w-7xl mx-auto mt-20 my-8 bg-gray-100 shadow-xl rounded-xl p-4">
+    <div className="flex space-x-2">
       <button
-        className={`text-sm px-4 py-2 rounded-md transition duration-300 ease-in-out transform hover:-translate-y-1 hover:shadow-md ${
-          selectedCategory === null ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-700'
+        className={`text-sm px-4 py-2 rounded-full transition-all duration-300 ease-in-out transform hover:scale-105 hover:bg-blue-600 ${
+          selectedCategory === null ? 'bg-gradient-to-r from-blue-500 to-indigo-500 text-white shadow-lg' : 'bg-gray-200 text-gray-700 hover:shadow-md'
         }`}
         onClick={() => setSelectedCategory(null)}
       >
@@ -122,8 +125,8 @@ const CategoryButtons: React.FC<CategoryButtonsProps> = ({ categories, setSelect
       {categories.map(category => (
         <button
           key={category}
-          className={`text-sm px-4 py-2 rounded-md transition duration-300 ease-in-out transform hover:-translate-y-1 hover:shadow-md ${
-            selectedCategory === category ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-700'
+          className={`text-sm px-4 py-2 rounded-full transition-all duration-300 ease-in-out transform hover:scale-105 ${
+            selectedCategory === category ? 'bg-gradient-to-r from-blue-500 to-indigo-500 text-white shadow-lg' : 'bg-gray-200 text-gray-700 hover:shadow-md hover:bg-gray-300'
           }`}
           onClick={() => setSelectedCategory(category)}
         >
@@ -141,7 +144,7 @@ interface ContentGridProps {
 const ContentGrid: React.FC<ContentGridProps> = ({ content }) => (
   <div className="w-full max-w-6xl grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-6">
     {content.map((item, index) => (
-      <ContentItemDisplay key={item.id + index} item={item} />
+      <ContentItemDisplay key={item.ID + index} item={item} />
     ))}
   </div>
 );
@@ -151,27 +154,37 @@ interface ContentItemDisplayProps {
 }
 
 const ContentItemDisplay: React.FC<ContentItemDisplayProps> = ({ item }) => (
-  <div className="bg-white rounded-lg overflow-hidden shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out">
+  <div className="relative bg-white rounded-lg overflow-hidden shadow-md hover:shadow-2xl transition-shadow duration-300 ease-in-out">
     {item.type === 'image' && (
       <>
-        <img src={item.image} alt="Content" className="w-full h-48 object-cover transition-transform duration-500 ease-in-out hover:scale-110" />
+        <img src={item.image} alt="Content" className="w-full h-64 object-cover transition-transform duration-500 ease-in-out hover:scale-110" />
         <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black to-transparent p-4">
-          <p className="text-white text-lg font-bold">{item.author}</p>
+          <p className="text-white text-lg font-bold truncate">{item.author}</p>
         </div>
       </>
     )}
+
+    {item.type === 'text' && (
+      <div className="p-6">
+        <a href={`/${item.Category.toUpperCase()}S/${item.ID}`}><p className="text-lg font-semibold text-gray-800 leading-tight">{item.content}</p></a>
+        <CatList cats={item.tags} />
+      </div>
+    )}
+
+    {item.type === 'video' && (
+      <>
+        <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black via-transparent to-transparent p-4">
+          <p className="text-white text-lg font-bold truncate">{item.author}</p>
+        </div>
+      </>
+    )}
+
     <div className="p-6">
-      {item.type === 'text' && (
-        <>
-          <p className="text-lg font-semibold text-gray-800">{item.content}</p>
-          <CatList cats={item.tags} />
-        </>
-      )}
       <div className="text-xs text-gray-500 mt-4 flex items-center">
-        <img src={item.authorAvatar} alt="Author Avatar" className="w-10 h-10 rounded-full mr-3" />
+        <img src={item.authorAvatar} alt="Author Avatar" className="w-10 h-10 rounded-full object-cover mr-3 shadow" />
         <div>
-          <p className="font-semibold">{item.author}</p>
-          <p>{item.date}</p>
+          <p className="font-semibold text-gray-700">{item.author}</p>
+          <p className="text-gray-400">{item.date}</p>
         </div>
       </div>
     </div>
@@ -184,10 +197,16 @@ interface TagListProps {
 
 const CatList: React.FC<TagListProps> = ({ cats }) => (
   <div className="mt-6 flex flex-wrap">
-    {cats.map(cat => (
-      <span key={cat} className="px-4 py-2 bg-blue-500 text-white rounded-full text-sm mb-3 mr-3">{cat}</span>
-    ))}
-  </div>
+  {cats.map(cat => (
+  <span key={cat} className="inline-block">
+  <a href={`/tags/${cat}`}
+     className="cursor-pointer px-5 py-2.5 leading-none text-black rounded-full text-sm mb-3 mr-3 shadow-md hover:shadow-lg transition duration-200 ease-in-out transform hover:scale-110 flex items-center justify-center">
+     <span className="text-slate-800">#</span>
+    <span className="rounded-full px-2 mr-2"> {cat} </span>
+  </a>
+</span>
+))}
+</div>
 );
 
 
