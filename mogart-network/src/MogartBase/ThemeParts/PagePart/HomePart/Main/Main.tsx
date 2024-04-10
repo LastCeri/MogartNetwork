bce1da5fe3e48e8e523954a519671d9a3e6ef2f7
LastCeri <125711498+LastCeri@users.noreply.github.com>
2024-04-10 21:22:22 +0300

    Part Update [93]
    Within the scope of the UI/UX Design Overhaul (Phase 9), Part Update [93] has been implemented. In this update, I separated the posts component within the Main component on the Home page. I made changes to the data coming from the API and updated the interface accordingly in PostProps. I also designed it to visually indicate posts that the user has previously liked, marking them accordingly.

--- mogart-network/src/MogartBase/ThemeParts/PagePart/HomePart/Main/Main.tsx
+++ mogart-network/src/MogartBase/ThemeParts/PagePart/HomePart/Main/Main.tsx
@@ -1,168 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPaperPlane,faThumbsUp,faShareNodes,faComment,faSliders } from '@fortawesome/free-solid-svg-icons';
-import { useFetchMogartPosts, createPost, PostSendLike, PostSendDislike, PostSendComment } from '../../../../Api/Api';
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+import { createPost, API_URL } from '../../../../Api/Api';
 import { useData } from '../../../../Context/DataContext';
-import ReactPlayer from 'react-player'
-import SharePopup from '../../../Popup/SharePopup';
-import { PostComments } from '../../../../Details/PostDetail/PostDetail';
-
-interface PostType {
-  Author: string;
-  Avatar: string;
-  GlobalId: string;
-  Content: string; 
-  Date: string;
-  CommentCount:string;
-  LikeCount:string;
-  VideoTitle?: string;
-  VideoDesc?: string;
-  ImageUrl?: string;
-  VideoUrl?: [];
-  PstComments: PostComments[];
-}
-
-function Post({ Author, Avatar, GlobalId, Content: PostContent, Date: PostDate, VideoTitle, VideoDesc,ImageUrl,VideoUrl,CommentCount,LikeCount,PstComments }: PostType): React.JSX.Element {
-  const [comments, setComments] = useState<PostComments[]>([]);
-  const [showCommentInput, setShowCommentInput] = useState(false);
-  const [showSharePopup, setShowSharePopup] = useState(false);
-  const [play, setPlay] = useState(false);
-  const { siteData, data } = useData();
-  const [commentText, setCommentText] = useState(""); 
-
-
-  const startVideo = () => {
-    setPlay(true);
-  };
-
-  const SendLike = async (globalId: string) => { await PostSendLike({UserID:data.UserName, ContentID:globalId, ContentType:"PostContent"}); };
-  const SendDisLike = async (globalId: string) => {  await PostSendDislike({UserID:data.UserName, ContentID:globalId, ContentType:"PostContent"}); };
-  const SendComment = async (globalId: string, commentcontent: string) => {await PostSendComment({UserID:data.UserName, ContentID:globalId, Content:commentcontent, ContentType:"PostContent"}); };
-
-  const handleCommentChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setCommentText(event.target.value);
-  };
-
-  const handleShareClick = () => {
-    setShowSharePopup(true);
-  };
-
-  const handleClosePopup = () => {
-    setShowSharePopup(false);
-  };
-
-  return (
-    
-    <div className="bg-white rounded-lg shadow-lg mb-8 p-4 text-gray-700 hover:shadow-xl transition-shadow duration-300">
-      <div className="flex items-center justify-between mb-3">
-        <Link to={`/posts/${GlobalId}`}>  
-        <div className="flex items-center no-underline text-gray-700">
-          <img className="h-8 w-8 rounded-full mr-2 object-cover" src={Avatar || siteData?.SiteDefaultProfileImageURL} alt={`${Author}'s avatar`} />
-            <div>
-              <div className="font-medium">{Author}</div>
-              <div className="text-xs text-gray-500">{PostDate}</div>
-            </div>
-        </div>
-        </Link>
-        <FontAwesomeIcon icon={faSliders} className="text-gray-500 hover:text-gray-700 cursor-pointer" />
-      </div>
-     
-      <p className="mb-3">{PostContent}</p>
-
-      {ImageUrl && !VideoUrl && (
-        <img src={ImageUrl} alt="Post" className="mb-3 max-w-full h-auto rounded-lg shadow" />
-      )}
-      
-      {VideoUrl && !ImageUrl &&(
-        <div className="flex justify-center items-center bg-black">
-        <div className="video-player-container bg-gray-800 rounded-lg overflow-hidden shadow-lg max-w-xl w-full">
-          <ReactPlayer
-            url={VideoUrl}
-            playing={play}
-            controls={true}
-            onStart={startVideo}
-            width="100%"
-            height="100%"
-            className="react-player rounded-lg"
-          />
-          <div className="p-4">
-            <h2 className="text-xl font-semibold text-white">{VideoTitle}</h2>
-            <p className="text-gray-400">{VideoDesc}</p>
-          </div>
-        </div>
-      </div>
-      )}
-
-      <div className="border-t pt-3 mt-3 text-sm flex items-center justify-between">
-        <div className="flex items-center space-x-4">
-          <button type="button" onClick={() => SendLike(GlobalId)} className="flex items-center text-gray-600 hover:text-blue-700 transition-colors duration-200 ease-in-out">
-            <FontAwesomeIcon icon={faThumbsUp} className="text-xl mr-2" /> 
-            <span className="font-medium">{LikeCount} Like</span>
-          </button>
-          <button type="button" onClick={() => setShowCommentInput(!showCommentInput)} className="flex items-center text-gray-600 hover:text-green-700 transition-colors duration-200 ease-in-out">
-            <FontAwesomeIcon icon={faComment} className="text-xl mr-2" />
-            <span className="font-medium">{CommentCount} Comment</span>
-          </button>
-        </div>
-        <button type="button" onClick={handleShareClick} className="flex items-center text-gray-600 hover:text-red-700 transition-colors duration-200 ease-in-out">
-          <FontAwesomeIcon icon={faShareNodes} className="text-xl mr-2" /> 
-          <span className="font-medium">Share</span>
-        </button>
-      </div>
-
-
-      {showCommentInput && (
-        <div className="mt-6 flex items-center space-x-2">
-          <input
-            type="text"
-            value={commentText}
-            onChange={handleCommentChange}
-            placeholder="Type a comment..."
-            className="w-full px-4 py-2 text-sm text-gray-700 bg-white border-2 border-gray-200 rounded-lg focus:ring-blue-500 focus:border-blue-500 transition duration-150 ease-in-out"
-            autoFocus
-          />
-          <button
-            className="px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded-lg transition duration-150 ease-in-out flex-shrink-0"
-            onClick={() => SendComment(GlobalId, commentText)}
-          >
-            <FontAwesomeIcon icon={faPaperPlane} />
-          </button>
-        </div>
-        )}
-        <div className="space-y-4 mt-6">
-        {comments.map((comment) => (
-          <div key={comment.comment_id} className="flex">
-            <div className="flex-shrink-0">
-              <img src={comment.profile_image} alt={comment.author} className="h-10 w-10 rounded-full object-cover" />
-            </div>
-            <div className="ml-3">
-              <div className="text-sm text-gray-800">{comment.content}</div>
-              <div className="flex items-center mt-2 space-x-2 text-xs text-gray-500">
-                <button className="flex items-center space-x-1 hover:text-blue-500">
-                  <FontAwesomeIcon icon={faThumbsUp} />
-                  <span>{1}</span>
-                </button>
-                <button className="flex items-center space-x-1 hover:text-green-500">
-                  <FontAwesomeIcon icon={faComment} />
-                  <span>{1}</span>
-                </button>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
-
-      {showSharePopup && <SharePopup url={`https://mogart-network.vercel.app/posts/${GlobalId}`} title={Author} onClose={handleClosePopup} />}
-    </div>
-  );
-}
+import axios from 'axios';
+import Posts, { PostProps } from './components/Posts';
 
 function MainContent() {
-  const { isLoggedIn, data, isLoading,siteData,userAuthID,userAuthToken } = useData();
+  const { isLoggedIn, data, isLoading, siteData, userAuthID, userAuthToken } = useData();
   const [postContent, setPostContent] = useState('');
-  const postsFromApi = useFetchMogartPosts();
-  const [posts, setPosts] = useState<PostType[]>([]);
+  const [posts, setPosts] = useState<PostProps[]>([]);
 
   const handlePostButtonClick = async () => {
     if (postContent.trim() !== '') {
@@ -183,14 +30,43 @@ function MainContent() {
     }
 };
 
-  useEffect(() => {
-    if (isLoading) {
-      return;
+useEffect(() => {
+  if (isLoading) return;
+
+  const fetchPosts = async () => {
+    try {
+      const { data } = await axios.get(`${API_URL}/GetMogartPosts`);
+      const mappedPosts: PostProps[] = data.map((post: any) => ({
+        GlobalId: post.Pstid,
+        Author: post.PstAuthor,
+        Avatar: post.PstAuthorAvatar,
+        Content: post.PstContent,
+        VideoUrl: post.PstVideos,
+        ImageUrl: post.PstImages,
+        CommentCount: post.PstCommentCount,
+        Date: post.PstDate,
+        DisLike: post.PstDisLike,
+        Mentions: post.PstMentions,
+        Name: post.PstName,
+        Points: post.PstPoints,
+        PostCode: post.PstPostCode,
+        Space: post.PstSpace,
+        Title: post.PstTitle,
+        VideoTitle: post.PstTitle,
+        VideoDesc: post.PstContent,
+        Views: post.PstViews,
+        Likes: typeof post.PstLike === 'string' ? JSON.parse(post.PstLike) : post.PstLike,
+        Comments: typeof post.PstComments === 'string' ? JSON.parse(post.PstComments) : post.PstComments,
+      }));
+      setPosts(mappedPosts);
+    } catch (error) {
+      console.error('Error fetching posts:', error);
     }
-    setPosts(postsFromApi);
-  }, [postsFromApi]);
-  
-  
+  };
+
+  fetchPosts();
+}, [isLoading]);
+
   return (
     <main className="w-1/2 sm:w-3/6 top-20 mr-16" style={{ marginRight: '20px' }}> 
       {isLoggedIn && (
@@ -223,7 +99,7 @@ function MainContent() {
       </div>
       <div className="space-y-4">
       {posts.map((post,key) => (
-          <Post
+          <Posts
             key={key}
             GlobalId={post.GlobalId}
             Author={post.Author}
@@ -234,8 +110,8 @@ function MainContent() {
             VideoTitle={post.VideoTitle}
             VideoDesc={post.VideoDesc}
             CommentCount={post.CommentCount}
-            LikeCount={post.LikeCount}
-            PstComments={post.PstComments}
+            Likes={post.Likes}
+            Comments={post?.Comments}
           />
       ))}
       </div>
