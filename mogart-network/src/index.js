c6283335085648117bdaf9172c253cdda08e7c81
LastCeri <125711498+LastCeri@users.noreply.github.com>
2024-05-14 17:16:29 +0300

    Update Context VoiceCallProvider

--- mogart-network/src/index.js
+++ mogart-network/src/index.js
@@ -4,7 +4,8 @@ import './index.css';
 import App from './App.tsx';
 import reportWebVitals from './reportWebVitals';
 import { DataProvider } from './MogartBase/Context/DataContext';
-import { VoiceCallProvider } from './MogartBase/Context/VoiceCallProvider';
+import { VoiceCallProvider } from './MogartBase/WebRTC/VoiceCallProvider.tsx';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
